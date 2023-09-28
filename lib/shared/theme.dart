7b06fd9398f6a249205415646b49bc3ea993978e
ahmdxhabibi <ahmdxhabibi@gmail.com>
2023-09-28 22:26:11 +0700

    add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// Color
+Color primaryColor = const Color(0xff5142E6);
+Color bgColor = const Color(0xffF6F8FB);
+Color bgDarkColor = const Color(0xff020518);
+Color blueColor = const Color(0xff53C1F9);
+Color greenColor = const Color(0xff22B07D);
+Color redColor = const Color(0xffFF1E39);
+Color blackColor = const Color(0xff14193F);
+Color whiteColor = const Color(0xffFFFFFF);
+Color greyColor = const Color(0xffA4A8AE);
+
+// TextStyle
+TextStyle blackTextStyle = TextStyle(
+  fontFamily: 'Poppins',
+  color: blackColor,
+);
+TextStyle greyTextStyle = TextStyle(
+  fontFamily: 'Poppins',
+  color: greyColor,
+);
+
+// FontWeight
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
