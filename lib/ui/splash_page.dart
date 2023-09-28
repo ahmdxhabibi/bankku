3beaab46742067ec08cb2bf57baa02e4635ac4da
ahmdxhabibi <ahmdxhabibi@gmail.com>
2023-09-28 22:28:02 +0700

    dark background

--- lib/ui/splash_page.dart
+++ lib/ui/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:bankku/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -6,6 +7,7 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgDarkColor,
       body: Center(
         child: Container(
           width: 155,
